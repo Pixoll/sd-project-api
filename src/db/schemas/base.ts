ce16c4015859f21e726f9dbd925f3d0fced878b6
Pixoll <pixoll.gamer@gmail.com>
2024-05-05 00:01:57 -0400

    Disable ESLint rules for entire file

--- src/db/schemas/base.ts
+++ src/db/schemas/base.ts
@@ -1,12 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
 import mongoose, { Model } from "mongoose";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export type JSONFromModel<M> = M extends Model<infer JSON, infer _2, infer _3, infer _4, infer _5, infer _6> ? JSON : never;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export type DocumentFromModel<M> = M extends Model<infer _1, infer _2, infer _3, infer _4, infer Doc, infer _6>
     ? Doc
     : never;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type SchemaTypeOptions = Record<string, mongoose.SchemaTypeOptions<any>> | {
     _id?: boolean;
 };
