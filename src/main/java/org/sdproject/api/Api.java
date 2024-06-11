980c6423ffc179a16f32910484bc25359ad66f08
Pixoll <pixoll.gamer@gmail.com>
2024-06-11 15:45:20 -0400

    Handle EndpointExceptions

--- src/main/java/org/sdproject/api/Api.java
+++ src/main/java/org/sdproject/api/Api.java
@@ -3,6 +3,8 @@ package org.sdproject.api;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.javalin.Javalin;
 import io.javalin.apibuilder.ApiBuilder;
+import io.javalin.http.Handler;
+import io.javalin.http.HttpStatus;
 import org.sdproject.api.endpoints.*;
 
 public class Api {
@@ -30,7 +32,7 @@ public class Api {
                     ));
                     config.jsonMapper(new JSONMapper());
                 })
-                .beforeMatched(Endpoint::beforeMatched)
+                .beforeMatched(wrapHandlerWithErrorHandler(Endpoint::beforeMatched))
                 .start(Integer.parseInt(DOTENV.get("PORT")));
     }
 
@@ -38,21 +40,43 @@ public class Api {
         for (final Endpoint endpoint : endpoints) {
             ApiBuilder.path(endpoint.path, () -> {
                 if (endpoint instanceof Endpoint.GetMethod method) {
-                    ApiBuilder.get(method::get);
+                    ApiBuilder.get(wrapHandlerWithErrorHandler(method::get));
                 }
                 if (endpoint instanceof Endpoint.PostMethod method) {
-                    ApiBuilder.post(method::post);
+                    ApiBuilder.post(wrapHandlerWithErrorHandler(method::post));
                 }
                 if (endpoint instanceof Endpoint.PutMethod method) {
-                    ApiBuilder.put(method::put);
+                    ApiBuilder.put(wrapHandlerWithErrorHandler(method::put));
                 }
                 if (endpoint instanceof Endpoint.PatchMethod method) {
-                    ApiBuilder.patch(method::patch);
+                    ApiBuilder.patch(wrapHandlerWithErrorHandler(method::patch));
                 }
                 if (endpoint instanceof Endpoint.DeleteMethod method) {
-                    ApiBuilder.delete(method::delete);
+                    ApiBuilder.delete(wrapHandlerWithErrorHandler(method::delete));
                 }
             });
         }
+    }
+
+    @SuppressWarnings("CallToPrintStackTrace")
+    private static Handler wrapHandlerWithErrorHandler(Handler handler) {
+        return (ctx) -> {
+            try {
+                handler.handle(ctx);
+            } catch (EndpointException e) {
+                ctx.status(e.getStatusCode()).json(e.toJSON());
+            } catch (RuntimeException e) {
+                if (e.getCause() instanceof EndpointException ee) {
+                    ctx.status(ee.getStatusCode()).json(ee.toJSON());
+                    return;
+                }
+
+                e.printStackTrace();
+                ctx.status(HttpStatus.INTERNAL_SERVER_ERROR);
+            } catch (Exception e) {
+                e.printStackTrace();
+                ctx.status(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        };
     }
 }
